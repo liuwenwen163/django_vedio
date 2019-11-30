2628b252c4d9d665f64bb6ac7778c2357888d674
liuwenwen163 <liuwenwen163@gamil.com>
2019-11-30 11:26:40 +0800

    9-2 完成了mako模板的初始化工作

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
 
 from django.urls import path
+from .views.base import Base
 
 urlpatterns = [
-
+    path('base', Base.as_view())
 ]
 
---
+++ video/app/libs/base_render.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+
+from mako.lookup import TemplateLookup
+from django.template import RequestContext
+from django.conf import settings
+from django.template.context import Context
+from django.http import HttpResponse
+
+
+def render_to_response(request, template, data=None):
+    """与jinjia2不同，mako的模板需要自己定义，于是有了本页的代码"""
+    context_instance = RequestContext(request)
+    path = settings.TEMPLATES[0]['DIRS'][0]
+
+    lookup = TemplateLookup(
+        directories=[path],
+        output_encoding='utf-8',
+        input_encoding='utf-8',
+    )
+
+    mako_template = lookup.get_template(template)
+
+    if not data:
+        data = {}
+
+    if context_instance:
+        context_instance.update(data)
+    else:
+        context_instance = Context(data)
+
+    result = {}
+
+    for d in context_instance:
+        result.update(d)
+
+    result['csrf_token'] = (
+        '<input type="hidden" '
+        'name="csrfmiddlewaretoken" '
+        'value="{0}" />'.format(
+            request.META.get('CSRF_COOKIE', '')),)
+
+    return HttpResponse(mako_template.render(**result))
+
